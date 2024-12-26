4978b7d31bef0f277c320bd871c1f39894f4300c
yusuke00001 <volleyball.pipi@icloud.com>
2024-12-26 14:43:53 +0900

    ユーザー認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   delete "/logout", to: "user_sessions#destroy"
 
   resources :users, only: %i[new create]
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
